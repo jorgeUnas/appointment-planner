80b0a435378f06bfa6696e918ffdaf99f24ddb19
EC2 Default User <ec2-user@ip-172-31-26-208.eu-north-1.compute.internal>
2024-03-25 13:54:00 +0000

    adding props to the contact page and variable states

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 
+
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
+export const ContactsPage = ({contacts, addContact}) => {
+  const [ name, setName ] = useState('');
+  const [ phone, setPhone ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ duplicate, setDuplicate ] = useState(false);  
   /*
   Define state variables for 
   contact info and duplicate check
