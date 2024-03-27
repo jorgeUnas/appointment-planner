15cc87b15871287ef49da37ebd625e1a50319048
EC2 Default User <ec2-user@ip-172-31-26-208.eu-north-1.compute.internal>
2024-03-27 14:30:55 +0000

    Adding a Tile list using props

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-export const TileList = () => {
+export const TileList = (props) => {
+  props.data.map((obj, index) => { 
+ 
   return (
     <div>
-      
+      <Tile name={obj.name} description='' key={index} />
     </div>
+    )}
   );
 };
