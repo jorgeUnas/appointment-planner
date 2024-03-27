515b8b224e16a1eacb56bb6fabbe164e2e407e06
EC2 Default User <ec2-user@ip-172-31-26-208.eu-north-1.compute.internal>
2024-03-27 15:54:37 +0000

    improving the tile list component

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = (props) => {
-  props.data.map((obj, index) => { 
- 
+export const TileList = ({ tiles }) => {
   return (
     <div>
-      <Tile name={obj.name} description='' key={index} />
+      {tiles.map((tile, index) => {
+        const {name, ...description } = tile;
+        return <Tile key={index} name={name} description={description}/>
+    })}
     </div>
-    )}
   );
 };
