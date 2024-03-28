33ca3e4f5090f25fe3bab9cfd1f902e0ae910de5
EC2 Default User <ec2-user@ip-172-31-26-208.eu-north-1.compute.internal>
2024-03-28 10:20:52 +0000

    creating the state variables and the handleSubmit for the app page component

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -15,7 +15,7 @@ export const ContactForm = ({
     <input id="name" type="text" value={name} onChange={({target}) => setName(target.value)} />
     <input id="phone" type="tel" value={phone} onChange={({target}) => setPhone(target.value)}  
           // regex is for US phone numbers
-          pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"/>
+          />
     <input id="email" type="email" value={email} onChange={({target}) => setEmail(target.value)} />
     <input type="submit" value="Add Contact" />
     </form>
